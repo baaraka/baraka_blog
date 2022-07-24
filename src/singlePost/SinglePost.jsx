a97919b623a8a4e8255aed5c7ed936a9c7010e97
baraka <barakaganai01@gmail.com>
2022-07-24 11:32:18 +0300

    create and write singlePost jsx component

---
+++ src/singlePost/SinglePost.jsx
@@ -0,0 +1,55 @@
+import "./singlepost.css";
+
+export default function SinglePost() {
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img
+          src="https://img.freepik.com/premium-photo/male-internet-hacker-hood-sitting-screens-back-view-illegal-web-programmer-workplace-criminal-occupation-data-hacking-cyber-security_266732-18744.jpg"
+          alt=""
+          className="singlePostImg"
+        />
+        <h1 className="singlePostTitle">
+          Javascript
+          <div className="singlePostEdit">
+            <i className="singlePostIcon editIcon fa-solid fa-pen-to-square"></i>
+            <i className="singlePostIcon deleteIcon fa-solid fa-trash-can"></i>
+          </div>
+        </h1>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Author: <b>Barack</b>
+          </span>
+          <span className="singlePostDate">1 hour ago</span>
+        </div>
+        <p className="singlePostDesc">
+          I am a full-stack web developer with strong knowledge of MongoDB,
+          node.js, Javascript and front end technologies such react, jquery, Ejs
+          and counting.Graduated University with degree in science of
+          information technology. Strong mathematics and programming
+          background.I am a full-stack web developer with strong knowledge of
+          MongoDB, node.js, Javascript and front end technologies such react,
+          jquery, Ejs and counting.Graduated University with degree in science
+          of information technology. Strong mathematics and programming
+          background.I am a full-stack web developer with strong knowledge of
+          MongoDB, node.js, Javascript and front end technologies such react,
+          jquery, Ejs and counting.Graduated University with degree in science
+          of information technology. Strong mathematics and programming
+          background.I am a full-stack web developer with strong knowledge of
+          MongoDB, node.js, Javascript and front end technologies such react,
+          jquery, Ejs and counting.Graduated University with degree in science
+          of information technology. Strong mathematics and programming
+          background.I am a full-stack web developer with strong knowledge of
+          MongoDB, node.js, Javascript and front end technologies such react,
+          jquery, Ejs and counting.Graduated University with degree in science
+          of information technology. Strong mathematics and programming
+          background.I am a full-stack web developer with strong knowledge of
+          MongoDB, node.js, Javascript and front end technologies such react,
+          jquery, Ejs and counting.Graduated University with degree in science
+          of information technology. Strong mathematics and programming
+          background.
+        </p>
+      </div>
+    </div>
+  );
+}
