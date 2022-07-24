692bff67167fa7c99d4d59ac85596fb029b17b0c
baraka <barakaganai01@gmail.com>
2022-07-24 11:30:39 +0300

    replace home component with single page component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
 import TopBar from "./topbar/TopBar";
 
 function App() {
   return (
     <>
     <TopBar/>
-    <Home/>
+    <Single/>
     </>
   );
 }
