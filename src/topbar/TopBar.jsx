8850ea45e8b8f6a822b848f8a87f52b559ccdd42
baraka <barakaganai01@gmail.com>
2022-07-25 06:11:40 +0300

    import link to topbar components

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -1,30 +1,65 @@
-import "./topbar.css"
+import { Link } from "react-router-dom";
+import "./topbar.css";
 
 export default function TopBar() {
-    return (
-        <div className="top">
-            <div className="topLeft">
-            <i className="topIcon fa-brands fa-facebook"></i>
-            <i className="topIcon fa-brands fa-twitter"></i>
-            <i className="topIcon fa-brands fa-instagram"></i>
-            <i className="topIcon fa-brands fa-linkedin"></i>
-            </div>
-            <div className="topCenter">
-                <ul className="topList">
-                    <li className="topListItem">HOME</li>
-                    <li className="topListItem">ABOUT</li>
-                    <li className="topListItem">CONTACT</li>
-                    <li className="topListItem">WRITE</li>
-                    <li className="topListItem">LOGOUT</li>
-                </ul>
-            </div>
-            <div className="topRight">
-                <img 
-                className="topImg" 
-                src="https://images.unsplash.com/photo-1519669417670-68775a50919c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGhhY2tlcnxlbnwwfHwwfHw%3D&w=1000&q=80" alt="" />
-                <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
-            </div>
+  const user = false;
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fa-brands fa-facebook"></i>
+        <i className="topIcon fa-brands fa-twitter"></i>
+        <i className="topIcon fa-brands fa-instagram"></i>
+        <i className="topIcon fa-brands fa-linkedin"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              CONTACT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          <li className="topListItem">{user && "LOGOUT"}</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <img
+            className="topImg"
+            src="https://images.unsplash.com/photo-1519669417670-68775a50919c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGhhY2tlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+            alt=""
+          />
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
 
-        </div>
-    )
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  );
 }
