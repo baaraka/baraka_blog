80aec71e2636eb317fddebb6bde3930398d28d91
baraka <barakaganai01@gmail.com>
2022-07-25 04:05:55 +0300

    create register page

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,19 @@
+import "./register.css";
+
+export default function Register() {
+  return (
+    <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerForm">
+        <label className="registerLabel">Username..</label>
+        <input type="text" placeholder="Enter Your Username.." />
+        <label className="registerLabel">E-mail</label>
+        <input type="email" placeholder="Enter Your Email.." />
+        <label className="registerLabel">Password</label>
+        <input type="password" placeholder="Enter Your Password.." />
+        <button className="registerButton">Register</button>
+      </form>
+      <button className="registerLoginButton">Login</button>
+    </div>
+  );
+}
