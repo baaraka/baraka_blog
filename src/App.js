3162ebe5ae0593b6cc9727a1566cdf7632d23c63
baraka <barakaganai01@gmail.com>
2022-07-25 04:04:08 +0300

    replace login page with register page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Setting from "./pages/settings/Setting";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
@@ -8,7 +10,7 @@ function App() {
   return (
     <>
     <TopBar/>
-    <Setting/>
+    <Register/>
     </>
   );
 }
