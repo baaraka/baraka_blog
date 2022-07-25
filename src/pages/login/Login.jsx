6dc610444f3291647a2024c910d7c7ea4276b163
baraka <barakaganai01@gmail.com>
2022-07-25 04:04:35 +0300

    create login page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,17 @@
+import "./login.css";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <span className="loginTitle">Login</span>
+      <form className="loginForm">
+        <label className="loginLabel">E-mail</label>
+        <input type="email" placeholder="Enter Your Email.." />
+        <label className="loginLabel">Password</label>
+        <input type="password" placeholder="Enter Your Password.." />
+        <button className="loginButton">Login</button>
+      </form>
+      <button className="loginRegisterButton">Register</button>
+    </div>
+  );
+}
