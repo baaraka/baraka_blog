8ed7f8c4bbbe10995dfc177e9f19ccbc354acb04
barakaganai <barakaganai01@gmail.com>
2023-06-22 17:24:51 +0300

    fetch all posts

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,23 +1,44 @@
-import "./post.css"
+import UseFetch from "../../hooks/UseFetch";
+import "./post.css";
+import { formatDistanceToNow } from "date-fns";
 
 export default function Post() {
+  const { data, loading, error } = UseFetch("http://localhost:8000/api/posts");
+
   return (
-    <div className="post">
-        <img src="https://img.freepik.com/premium-photo/male-internet-hacker-hood-sitting-screens-back-view-illegal-web-programmer-workplace-criminal-occupation-data-hacking-cyber-security_266732-18744.jpg" alt="" className="postImg" />
-        <div className="postInfo">
-            <div className="postCats">
-                <span className="postCat">music</span>
-                <span className="postCat">life</span>
-            </div>
-            <span className="postTitle">Javascript</span>
-            <hr />
-            <span className="postDate">1 hour ago</span>
-        </div>
-        <p className="postDescription">
-        I am a full-stack web developer with strong knowledge of MongoDB, node.js, Javascript and front end technologies such react, jquery, Ejs and counting.Graduated University with degree in science of information technology. Strong mathematics and programming background.
-        I am a full-stack web developer with strong knowledge of MongoDB, node.js, Javascript and front end technologies such react, jquery, Ejs and counting.Graduated University with degree in science of information technology. Strong mathematics and programming background.
-        I am a full-stack web developer with strong knowledge of MongoDB, node.js, Javascript and front end technologies such react, jquery, Ejs and counting.Graduated University with degree in science of information technology. Strong mathematics and programming background.
-        </p>
-    </div>
-  )
+    <>
+      {loading ? (
+        "please wait..."
+      ) : (
+        <>
+          {data.map((item) => (
+            <>
+              <div className="post">
+                <img src={item.photo} alt="" className="postImg" />
+                <div className="postInfo">
+                  <div className="postCats">
+                    {item.categories.map((category) => (
+                      <span className="postCat" key={category}>
+                        {category}
+                      </span>
+                    ))}
+                  </div>
+                  <span className="postTitle">{item.title}</span>
+                  <hr />
+                  <span className="postDate">
+                    <span className="postDate">
+                      {formatDistanceToNow(new Date(item.createdAt), {
+                        addSuffix: true,
+                      })}
+                    </span>
+                  </span>
+                </div>
+                <p className="postDescription">{item.desc}</p>
+              </div>
+            </>
+          ))}
+        </>
+      )}
+    </>
+  );
 }
