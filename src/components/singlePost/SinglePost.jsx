87b889000b466911c7999e1e2a39f5d7519f2152
barakaganai <barakaganai01@gmail.com>
2023-06-23 15:23:41 +0300

    fetch data from the database

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -1,16 +1,15 @@
+import { useParams } from "react-router-dom";
+import UseFetch from "../../hooks/UseFetch";
 import "./singlepost.css";
+import { formatDistanceToNow } from "date-fns";
 
-export default function SinglePost() {
+export default function SinglePost({ item }) {
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
-        <img
-          src="https://img.freepik.com/premium-photo/male-internet-hacker-hood-sitting-screens-back-view-illegal-web-programmer-workplace-criminal-occupation-data-hacking-cyber-security_266732-18744.jpg"
-          alt=""
-          className="singlePostImg"
-        />
+        <img src={item?.photo} alt="" className="singlePostImg" />
         <h1 className="singlePostTitle">
-          Javascript
+          {item?.title}
           <div className="singlePostEdit">
             <i className="singlePostIcon editIcon fa-solid fa-pen-to-square"></i>
             <i className="singlePostIcon deleteIcon fa-solid fa-trash-can"></i>
@@ -20,35 +19,9 @@ export default function SinglePost() {
           <span className="singlePostAuthor">
             Author: <b>Barack</b>
           </span>
-          <span className="singlePostDate">1 hour ago</span>
+          <span className="singlePostDate">{item?.createdAt}</span>
         </div>
-        <p className="singlePostDesc">
-          I am a full-stack web developer with strong knowledge of MongoDB,
-          node.js, Javascript and front end technologies such react, jquery, Ejs
-          and counting.Graduated University with degree in science of
-          information technology. Strong mathematics and programming
-          background.I am a full-stack web developer with strong knowledge of
-          MongoDB, node.js, Javascript and front end technologies such react,
-          jquery, Ejs and counting.Graduated University with degree in science
-          of information technology. Strong mathematics and programming
-          background.I am a full-stack web developer with strong knowledge of
-          MongoDB, node.js, Javascript and front end technologies such react,
-          jquery, Ejs and counting.Graduated University with degree in science
-          of information technology. Strong mathematics and programming
-          background.I am a full-stack web developer with strong knowledge of
-          MongoDB, node.js, Javascript and front end technologies such react,
-          jquery, Ejs and counting.Graduated University with degree in science
-          of information technology. Strong mathematics and programming
-          background.I am a full-stack web developer with strong knowledge of
-          MongoDB, node.js, Javascript and front end technologies such react,
-          jquery, Ejs and counting.Graduated University with degree in science
-          of information technology. Strong mathematics and programming
-          background.I am a full-stack web developer with strong knowledge of
-          MongoDB, node.js, Javascript and front end technologies such react,
-          jquery, Ejs and counting.Graduated University with degree in science
-          of information technology. Strong mathematics and programming
-          background.
-        </p>
+        <p className="singlePostDesc">{item?.desc}</p>
       </div>
     </div>
   );
