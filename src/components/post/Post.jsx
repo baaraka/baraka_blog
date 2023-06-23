1febb2da05c819ad4663714e33da2fca24aacffc
barakaganai <barakaganai01@gmail.com>
2023-06-23 15:24:26 +0300

    fetch data from mongoDB

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import UseFetch from "../../hooks/UseFetch";
 import "./post.css";
 import { formatDistanceToNow } from "date-fns";
+import SinglePost from "../singlePost/SinglePost";
 
 export default function Post() {
   const { data, loading, error } = UseFetch("http://localhost:8000/api/posts");
@@ -9,32 +11,40 @@ export default function Post() {
     <>
       {loading ? (
         "please wait..."
+      ) : error ? (
+        "error in fetching data"
       ) : (
         <>
           {data.map((item) => (
             <>
-              <div className="post">
-                <img src={item.photo} alt="" className="postImg" />
-                <div className="postInfo">
-                  <div className="postCats">
-                    {item.categories.map((category) => (
-                      <span className="postCat" key={category}>
-                        {category}
-                      </span>
-                    ))}
-                  </div>
-                  <span className="postTitle">{item.title}</span>
-                  <hr />
-                  <span className="postDate">
+              <Link
+                to={`/posts/${item._id}`}
+                key={item._id}
+                style={{ color: "inherit", textDecoration: "none" }}
+              >
+                <div className="post" key={item._id}>
+                  <img src={item.photo} alt="" className="postImg" />
+                  <div className="postInfo">
+                    <div className="postCats">
+                      {item.categories.map((category) => (
+                        <span className="postCat" key={category}>
+                          {category}
+                        </span>
+                      ))}
+                    </div>
+                    <span className="postTitle">{item.title}</span>
+                    <hr />
                     <span className="postDate">
-                      {formatDistanceToNow(new Date(item.createdAt), {
-                        addSuffix: true,
-                      })}
+                      <span className="postDate">
+                        {formatDistanceToNow(new Date(item.createdAt), {
+                          addSuffix: true,
+                        })}
+                      </span>
                     </span>
-                  </span>
+                  </div>
+                  <p className="postDescription">{item.desc}</p>
                 </div>
-                <p className="postDescription">{item.desc}</p>
-              </div>
+              </Link>
             </>
           ))}
         </>
