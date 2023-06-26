d5be19777c3282d5192ebd6e050257d60175ffe4
barakaganai <barakaganai01@gmail.com>
2023-06-26 14:26:48 +0300

    fetch data using axios

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,22 +1,25 @@
+import React from "react";
 import Post from "../post/Post";
 import "./posts.css";
+import UseFetch from "../../hooks/UseFetch";
+import SinglePost from "../singlePost/SinglePost";
 
 export default function Posts() {
+  const { data, loading, error } = UseFetch("http://localhost:8000/api/posts");
+
   return (
     <div className="posts">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {loading ? (
+        "loading please wait..."
+      ) : error ? (
+        "Error in fetching data.."
+      ) : (
+        <>
+          {data.map((item) => (
+            <Post item={item} key={item.id} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
