a3d7887164d4089be7613ddd1bccee782189f177
barakaganai <barakaganai01@gmail.com>
2023-06-29 21:49:12 +0300

    update posts.jsx

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,25 +1,11 @@
 import React from "react";
 import Post from "../post/Post";
 import "./posts.css";
-import UseFetch from "../../hooks/UseFetch";
-import SinglePost from "../singlePost/SinglePost";
 
 export default function Posts() {
-  const { data, loading, error } = UseFetch("http://localhost:8000/api/posts");
-
   return (
     <div className="posts">
-      {loading ? (
-        "loading please wait..."
-      ) : error ? (
-        "Error in fetching data.."
-      ) : (
-        <>
-          {data.map((item) => (
-            <Post item={item} key={item.id} />
-          ))}
-        </>
-      )}
+      <Post />
     </div>
   );
 }
