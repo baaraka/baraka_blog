ec6b0e685dc186d8da27f91d84cfd3cd4e40c571
barakaganai <barakaganai01@gmail.com>
2023-06-30 22:37:14 +0300

    import  useContext

--- src/pages/settings/Setting.jsx
+++ src/pages/settings/Setting.jsx
@@ -1,7 +1,13 @@
+import { useContext, useState } from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./setting.css";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Setting() {
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  const handleChange = (e) => {};
+
   return (
     <div className="setting">
       <div className="settingWrapper">
@@ -22,12 +28,24 @@ export default function Setting() {
             <input type="file" id="fileInput" style={{ display: "none" }} />
           </div>
           <label>Username</label>
-          <input type="text" placeholder="Barack" />
+          <input
+            type="text"
+            id="username"
+            placeholder="Barack"
+            onChange={handleChange}
+          />
           <label>E-mail</label>
-          <input type="email" placeholder="barakaganai01@gmail.com" />
+          <input
+            type="email"
+            id="email"
+            placeholder="barakaganai01@gmail.com"
+            onChange={handleChange}
+          />
           <label>Password</label>
-          <input type="password" />
-          <button className="settingSubmit">Update</button>
+          <input type="password" id="password" onChange={handleChange} />
+          <button disabled={loading} className="settingSubmit">
+            Update
+          </button>
         </form>
       </div>
       <Sidebar />
