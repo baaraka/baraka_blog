6584c4b3fe56d79f686fa76ac3127bfc22794a93
barakaganai <barakaganai01@gmail.com>
2023-07-01 10:54:02 +0300

    enable fetch data from the db

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,29 @@ import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.css";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const { search } = useLocation();
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("http://localhost:8000/api/posts" + search);
+      setPosts(res.data);
+    };
+    fetchData();
+  }, [search]);
+
   return (
     <>
       <Header />
       <div className="home">
-        <Posts />
+        <Posts posts={posts} />
+
         <Sidebar />
       </div>
     </>
