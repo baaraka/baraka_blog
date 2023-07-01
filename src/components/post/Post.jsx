5e7bdd09fdcfa79229ca4ff3de8c4583ac56494d
barakaganai <barakaganai01@gmail.com>
2023-07-01 10:55:01 +0300

    update post.jsx

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,36 +1,34 @@
 import { Link } from "react-router-dom";
 import "./post.css";
-import { formatDistanceToNow } from "date-fns";
 
 export default function Post({ item }) {
+  const pf = "http://localhost:8000/images/";
   return (
-    <Link
-      to={`/posts/${item._id}`}
-      key={item._id}
-      style={{ color: "inherit", textDecoration: "none" }}
-    >
-      <div className="post" key={item._id}>
-        <img src={item.photo} alt="" className="postImg" />
-        <div className="postInfo">
-          <div className="postCats">
-            {item.categories.map((category) => (
-              <span className="postCat" key={category}>
-                {category}
-              </span>
-            ))}
-          </div>
-          <span className="postTitle">{item.title}</span>
-          <hr />
-          <span className="postDate">
-            <span className="postDate">
-              {formatDistanceToNow(new Date(item.createdAt), {
-                addSuffix: true,
-              })}
+    <div className="post">
+      {item.photo && <img src={pf + item.photo} alt="" className="postImg" />}
+      <div className="postInfo">
+        <div className="postCats">
+          {item.categories.map((category) => (
+            <span className="postCat" key={category}>
+              {category}
             </span>
-          </span>
+          ))}
         </div>
-        <p className="postDescription">{item.desc}</p>
+        <span className="postTitle">{item.title}</span>
+        <hr />
+        <span className="postDate">
+          <span className="postDate">
+            {new Date(item.createdAt).toDateString()}
+          </span>
+        </span>
       </div>
-    </Link>
+      <Link
+        to={`/posts/${item._id}`}
+        key={item._id}
+        style={{ color: "inherit", textDecoration: "none" }}
+      >
+        <p className="postDescription">{item.desc}</p>
+      </Link>
+    </div>
   );
 }
