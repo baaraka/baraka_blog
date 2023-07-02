c81c723823dbf9ed773cfda19b3013a29414215c
barakaganai <barakaganai01@gmail.com>
2023-07-02 13:16:35 +0300

    enable updateUser and deleteUser

--- src/pages/settings/Setting.jsx
+++ src/pages/settings/Setting.jsx
@@ -2,50 +2,114 @@ import { useContext, useState } from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./setting.css";
 import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Setting() {
-  const { loading, error, dispatch } = useContext(AuthContext);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [file, setFile] = useState("");
+  const [message, setMessage] = useState(false);
 
-  const handleChange = (e) => {};
+  const pf = "http://localhost:8000/images/";
+
+  const { user, dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handlePublish = async (e) => {
+    e.preventDefault();
+
+    dispatch({ type: "UPDATE_START" });
+    const updatedUser = {
+      userId: user._id,
+      username,
+      email,
+      password,
+    };
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      updatedUser.profilePic = filename;
+
+      try {
+        await axios.post("http://localhost:8000/api/upload", data);
+      } catch (error) {}
+    }
+
+    try {
+      const res = await axios.put(
+        `http://localhost:8000/api/users/${user._id}`,
+        updatedUser
+      );
+      setMessage(true);
+      dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
+      navigate("/");
+    } catch (error) {
+      dispatch({ type: "UPDATE_FAILURE", payload: error.message });
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`http://localhost:8000/api/users/${user._id}`, {
+        data: { userId: user._id },
+      });
+      navigate("/");
+    } catch (error) {}
+  };
 
   return (
     <div className="setting">
       <div className="settingWrapper">
         <div className="settingTitle">
           <span className="settingUpdateTitle">Update Your Account</span>
-          <span className="settingDeleteTitle">Delete Account</span>
+          <span className="settingDeleteTitle" onClick={handleDelete}>
+            Delete Account
+          </span>
         </div>
-        <form className="settingForm">
+        <form className="settingForm" onSubmit={handlePublish}>
           <label>Profile Picture</label>
           <div className="settingPP">
             <img
-              src="https://img.freepik.com/premium-photo/male-internet-hacker-hood-sitting-screens-back-view-illegal-web-programmer-workplace-criminal-occupation-data-hacking-cyber-security_266732-18744.jpg"
+              src={file ? URL.createObjectURL(file) : pf + user.profilePic}
               alt=""
             />
             <label htmlFor="fileInput">
               <i className="settingPPIcon fa-solid fa-user"></i>
             </label>
-            <input type="file" id="fileInput" style={{ display: "none" }} />
+            <input
+              type="file"
+              id="fileInput"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
           </div>
           <label>Username</label>
           <input
             type="text"
             id="username"
-            placeholder="Barack"
-            onChange={handleChange}
+            placeholder={user.username}
+            onChange={(e) => setUsername(e.target.value)}
           />
           <label>E-mail</label>
           <input
             type="email"
             id="email"
-            placeholder="barakaganai01@gmail.com"
-            onChange={handleChange}
+            placeholder={user.email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label>Password</label>
-          <input type="password" id="password" onChange={handleChange} />
-          <button disabled={loading} className="settingSubmit">
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="settingSubmit" type="submit">
             Update
           </button>
+          {message && <span>Your profile has updated</span>}
         </form>
       </div>
       <Sidebar />
