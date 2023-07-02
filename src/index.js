c38b67aa28c89b1d9de454d7aae88e9d82d08d97
barakaganai <barakaganai01@gmail.com>
2023-07-02 13:15:34 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { WriteContextProvider } from "./context/WriteContext";
 import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <WriteContextProvider>
-        <App />
-      </WriteContextProvider>
+      <App />
     </AuthContextProvider>
   </React.StrictMode>
 );
